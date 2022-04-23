55f605c547d82548d726d6473c6292904bde97bf
caiocsgomes <caiocsgomes@gmail.com>
2022-04-23 15:04:09 -0300

    creating project (db): enforcing foreign key support for sqlite

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,3 +1,15 @@
+from sqlite3 import Connection as SQLite3Connection
+
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 
 db = SQLAlchemy()
+
+
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
