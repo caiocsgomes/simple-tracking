d191148031f89759bb0549a4ab0f0c6694e9188a
caiocsgomes <caiocsgomes@gmail.com>
2022-04-27 08:45:23 -0300

    creating project (pipeline): adding code pipeline role

---
+++ pipeline/codepipeline.tf
@@ -0,0 +1,32 @@
+resource aws_iam_role "pipeline_role" {
+  name               = format("%s-role", var.pipeline_name)
+  assume_role_policy = data.aws_iam_policy_document.pipeline_trust_policy_document.json
+}
+
+resource "aws_iam_role_policy" "pipeline_policy" {
+  name   = format("%s-policy", var.pipeline_name)
+  role   = aws_iam_role.pipeline_role.id
+  policy = data.aws_iam_policy_document.pipeline_policy_document.json
+}
+
+data "aws_iam_policy_document" "pipeline_policy_document" {
+  statement {
+    actions   = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"]
+    resources = ["*"]
+  }
+  statement {
+    actions   = ["codebuild:StartBuild", "codebuild:BatchGetBuilds"]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "pipeline_trust_policy_document" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
---
+++ pipeline/variables.tf
@@ -0,0 +1,1 @@
+variable "pipeline_name" {}
