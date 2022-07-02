94bd9371b693b1c6fd5663c8b15a2d9ad120565f
caiocsgomes <caiocsgomes@gmail.com>
2022-07-02 17:25:18 -0300

    creating app (tests): removing unitest dependency

--- app/tests/service/test_aws_secrets_manager.py
+++ app/tests/service/test_aws_secrets_manager.py
@@ -1,18 +1,16 @@
 import os
-from unittest import TestCase
-from unittest.mock import MagicMock
 
 import boto3
 from moto import mock_secretsmanager
+
 from src.service.aws_secrets_manager import SecretsManagerService
 
 
-class TestSecretsManagerService(TestCase):
-    mock_boto: MagicMock
+class TestSecretsManagerService:
     secrets_manager_service: SecretsManagerService
 
     @classmethod
-    def setUp(cls):
+    def setup_class(cls):
         cls.secrets_manager_service = SecretsManagerService()
         os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
         os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
@@ -21,7 +19,7 @@ class TestSecretsManagerService(TestCase):
         os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
 
     @mock_secretsmanager
-    def test_get_secret_with_moto(self):
+    def test_get_secret_with_moto(self, app):
         conn = boto3.client("secretsmanager", region_name="us-east-1")
         conn.create_secret(Name="test-secret", SecretString="test-password")
         response = self.secrets_manager_service.get_secret("test-secret")
