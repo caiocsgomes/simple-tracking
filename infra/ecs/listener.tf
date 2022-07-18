fc7bcce37a57ef0f59df12dca1267efd03361d4c
caiocsgomes <caiocsgomes@gmail.com>
2022-07-18 08:02:26 -0300

    creating project (infra): adding listener do alb

---
+++ infra/ecs/listener.tf
@@ -0,0 +1,17 @@
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_alb.cluster_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate_validation.cert_validation.certificate_arn
+
+  ## the default action should not be used, instead we use an action per service
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "route not implemented"
+      status_code  = "501"
+    }
+  }
+}
